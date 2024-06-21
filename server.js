e481c65523868258c7a8617f1607163722ca1243
Monti junior <montiabouou@gmail.com>
2024-06-21 02:16:52 -0800

    Server logic added..

--- server.js
+++ server.js
@@ -1,4 +1,39 @@
 const http = require("http");
 const fs = require("fs");
 
-const server = http.createServer();
+const server = http.createServer((req, res) => {
+  res.setHeader("content-Type", "text/html");
+
+  let path = ".";
+  switch (req.url) {
+    case "/":
+      path += "/index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "/about.html";
+      res.statusCode = 200;
+      break;
+    case "/contact":
+      path += "/contact.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "/404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.error(err);
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+});
+
+server.listen(3000, "localhost", () => {
+  console.log("Server listening on port 3000");
+});
