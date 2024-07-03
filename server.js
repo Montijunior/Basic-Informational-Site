0cac00b228f05a9c22a0701e78bb8206dc0b7f3b
Monti junior <montiabouou@gmail.com>
2024-07-03 00:09:27 -0800

    Add express app.get() and app.use request handlers for pages and error page

--- server.js
+++ server.js
@@ -2,6 +2,25 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 
+// Home route
+app.get("/", (req, res) => {
+  res.sendFile("index.html");
+});
+
+// About route
+app.get("/about", (req, res) => {
+  res.sendFile("about.html");
+});
+
+// Contact route
+app.get("/contact", (req, res) => {
+  res.sendFile("contact.html");
+});
+
+// 404 route
+app.use((req, res) => {
+  res.sendFile("404.html");
+});
 app.listen(3000, "localhost", () => {
   console.log("Server listening on port 3000");
 });
