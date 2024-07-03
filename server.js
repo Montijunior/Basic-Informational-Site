1121c0fdfa5b6d467fb2ed4996ec656957e1fe94
Monti junior <montiabouou@gmail.com>
2024-07-03 00:03:27 -0800

    Refactoring node.js code to express.

--- server.js
+++ server.js
@@ -1,39 +1,7 @@
-const http = require("http");
 const fs = require("fs");
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-  res.setHeader("content-Type", "text/html");
-
-  let path = ".";
-  switch (req.url) {
-    case "/":
-      path += "/index.html";
-      res.statusCode = 200;
-      break;
-    case "/about":
-      path += "/about.html";
-      res.statusCode = 200;
-      break;
-    case "/contact":
-      path += "/contact.html";
-      res.statusCode = 200;
-      break;
-    default:
-      path += "/404.html";
-      res.statusCode = 404;
-      break;
-  }
-
-  fs.readFile(path, (err, data) => {
-    if (err) {
-      console.error(err);
-    } else {
-      res.write(data);
-      res.end();
-    }
-  });
-});
-
-server.listen(3000, "localhost", () => {
+app.listen(3000, "localhost", () => {
   console.log("Server listening on port 3000");
 });
