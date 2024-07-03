0c5f371c8298ef742dd6adde759cb95a26f15e06
Monti junior <montiabouou@gmail.com>
2024-07-03 00:25:22 -0800

    Added path module to find files and return location

--- server.js
+++ server.js
@@ -1,25 +1,29 @@
 const fs = require("fs");
+const path = require("path");
 const express = require("express");
 const app = express();
 
 // Home route
 app.get("/", (req, res) => {
-  res.sendFile("index.html");
+  const file = path.join(__dirname, "./", "index.html");
+  res.sendFile(file);
 });
-
 // About route
 app.get("/about", (req, res) => {
-  res.sendFile("about.html");
+  const file = path.join(__dirname, "./", "about.html");
+  res.sendFile(file);
 });
 
 // Contact route
 app.get("/contact", (req, res) => {
-  res.sendFile("contact.html");
+  const file = path.join(__dirname, "./", "contact.html");
+  res.sendFile(file);
 });
 
 // 404 route
 app.use((req, res) => {
-  res.sendFile("404.html");
+  const file = path.join(__dirname, "./", "404.html");
+  res.sendFile(file);
 });
 app.listen(3000, "localhost", () => {
   console.log("Server listening on port 3000");
